e7a08181ba6c21cba77ae164dd5521864cbee49b
Strike24 <benxcohen@gmail.com>
2025-12-05 23:32:58 +0200

    DNS Sender first commit

---
+++ sender/sender.h
@@ -0,0 +1,7 @@
+#include <stdio.h>      // Standard Input/Output
+#include <stdlib.h>     // Standard Library
+#include <unistd.h>     // closing the socket
+#include <string.h>     // Memory handling
+#include <sys/socket.h> // main Socket API
+#include <netinet/in.h> // Definitions for IP addresses
+#include <arpa/inet.h>  // Utilities to convert IPs
